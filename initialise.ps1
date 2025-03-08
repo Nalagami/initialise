a3b3ac76a028e4b785097f8ba178caba762d52f4
nalagami <tnt5547@gmail.com>
2025-03-08 20:40:22 +0900

    関数ごとに分離

--- initialise.ps1
+++ initialise.ps1
@@ -1,41 +1,28 @@
-#########################
-# python の設定
-# version 3.11
-######################### 
-winget install --id Python.Python.3.11 --accept-package-agreements
+# -*- coding: utf-8 -*-
 
-#########################
-# gitの設定
-# 参老 https://blog.katsubemakito.net/git/git-config-1st 
-######################### 
+function Install-Python {
+    Write-Output "Installing pyenv..."
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+Write-Output "Installing the latest Python version using pyenv..."
+$latestPythonVersion = pyenv install --list | Select-String -Pattern "^\s*3\.\d+\.\d+$"| ForEach-Object { $_.Line.Trim() } | Where-Object { $_ -notmatch "-" -and $_ -notmatch "b" } | ForEach-Object { [Version]$_ } | Sort-Object -Descending | Select-Object -First 1
 
-# ユーザー名、メールアドレス
+pyenv install $latestPythonVersion.ToString()
+pyenv global $latestPythonVersion.ToString()
+}
+
+
+function Set-GitConfiguration {
+Write-Output "Configuring Git..."
 git config --global user.name 'nalagami'
 git config --global user.email 'tnt5547@gmail.com'
-
-# push方式
 git config --global push.default current
-
-# pull方式
 git config --global pull.ff only
 git config --global pull.rebase false
-
-# デフォルトブランチ名
 git config --global init.defaultBranch main
-
-# 改行コード自動変換設定
 git config --global core.autocrlf false
-
-# ファイル名 大文字小文字識別設定
 git config --global core.ignorecase false
-
-# ハイライト設定
 git config --global color.ui true
-
-# デフォルトエディタ
 git config --global core.editor 'code --wait'
-
-# エイリアス設定
 git config --global alias.st status
 git config --global alias.df diff
 git config --global alias.co commit
@@ -43,48 +30,52 @@ git config --global alias.br branch
 git config --global alias.ps push
 git config --global alias.pl pull
 git config --global alias.ck checkout
-
-# 日本語ファイル名をエンコードしない
 git config --global core.quotepath false
-
-# vacuumコマンドを追加
-# https://qiita.com/hajimeni/items/73d2155fc59e152630c4
 Add-Content -Path $HOME/.gitconfig -Encoding utf8 "[alias]`n`tvacuum = `"!f () { git checkout $1; git branch --merged|egrep -v '\\*|develop|main'|xargs git branch -d; git fetch --prune; };f`""
+}
 
-# gitの状態を表示するアプリをインストール
-# https://github.com/dahlbyk/posh-git/blob/master/README.md
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm
-Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-Add-PoshGitToProfile -AllHosts
+function Install-oh-my-posh {
+Write-Output "Installing oh-my-posh..."
+winget install --id JanDeDobbeleer.OhMyPosh --accept-package-agreements
+Install-Module posh-git -Scope CurrentUser -Force
+Import-Module posh-git
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+Set-PSReadLineOption -PredictionSource History
+Install-Module -Name Terminal-Icons
+Import-Module -Name Terminal-Icons
+}
 
-#########################
-# node.jsの設定
-######################### 
-
-# nvm インストール
+function Install-NodeJS {
+Write-Output "Installing Node.js..."
 winget install --id CoreyButler.NVMforWindows --accept-package-agreements
-# nvm install --lts
+nvm install lts
+nvm use lts
+}
 
-# git cz
-npm install -g git-cz
-
-#########################
-# aws cli の設定
-######################### 
-
-# aws cli インストール
+function Install-AWSCLI {
+Write-Output "Installing AWS CLI..."
 winget install --id Amazon.AWSCLI --accept-package-agreements
+}
 
-#########################
-# ServerlessFrameworkの設定
-######################### 
+function Install-AWSSAMCLI {
+Write-Output "Installing AWS SAM CLI..."
+winget install --id Amazon.SAMCLI --accept-package-agreements
+}
 
-# ServerlessFramework インストール
-npm install -g serverless
-
-#########################
-# Terraformの設定
-######################### 
-
-# terraform インストール
+function Install-Terraform {
+Write-Output "Installing Terraform..."
 winget install --id Hashicorp.Terraform --accept-package-agreements
+}
+
+# Main script execution
+Write-Output "Initializing the development environment..."
+Install-Pyenv
+Install-Python
+Set-GitConfiguration
+Install-oh-my-posh
+Install-NodeJS
+Install-AWSCLI
+Install-AWSSAMCLI
+Install-Terraform
+
+Write-Output "Initialization complete."
